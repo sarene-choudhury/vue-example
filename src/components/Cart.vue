d67202cb0f891cdfb3481d6c601bb21c9e3ba380
Sarene Syed <sarene@Sarenes-Air.fios-router.home>
2023-03-17 19:58:11 -0400

    submission

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,12 +1,22 @@
 <script>
 import NavBar from './NavBar.vue'
+import CartItem from '.CartItem.vue'
 
 
 export default {
     components: {
         NavBar,
+        CartItem,
+    },
+    data() {
+        return {
+            hotdog: "default",
+            itemCount: 0,
+        }
     }
 }
+
+
 
 </script>
 <template>
@@ -16,5 +26,40 @@ export default {
         <div class="links">
         </div>
 
+        <CartItem>
+                image = '../assets/vintage.png' 
+                productName = "Vintage Hot Dog"
+                description = "A hotdog dressed up in vintage clothing"
+                price = "2,000,000"
+
+                @add-to-cart="$event => itemCount++"
+        </CartItem>
+        <CartItem>
+                image = '../assets/patriotic.png' 
+                productName = "Patriotic Hot Dog"
+                description = "A hotdog patriotic enough for the fourth of july"
+                price = "10,000,000"
+
+                @add-to-cart="$event => itemCount++"
+        </CartItem>
+        <CartItem>
+                image = '../assets/beautiful.png' 
+                productName = "Beautiful Hot Dog"
+                description = "A hotdog all glammed up"
+                price = "1,000,000"
+
+                @add-to-cart="$event => itemCount++"
+        </CartItem>
+        <CartItem>
+                image = '../assets/winner.png' 
+                productName = "Winner Hot Dog"
+                description = "A hotdog who has beaten Michael Phelps and is proud of it"
+                price = "700,000,000"
+
+                @add-to-cart="$event => itemCount++"
+        </CartItem>
+
     </body>
+    The amount of hotdogs in your cart totals to {{ itemCount }}
+    
 </template>
---
+++ src/components/CartItem.vue
@@ -0,0 +1,33 @@
+<script>
+import NavBar from './NavBar.vue'
+
+
+export default{
+    props: [
+        "image",
+        "productName",
+        "description",
+        "price",
+    ],
+    methods: {
+        addItemToCart(productName) 
+        {this.$emit('addItemToCart', productName)}
+    },
+}
+
+</script>
+<template>
+    <body>
+        <NavBar></NavBar>
+        <img :src="src" alt="hotdog">
+        <div class="links">
+        </div>
+    </body>
+    <div class="item">
+        <h1>image: {{ image }}</h1>
+        <h2>productName: {{ productName }}</h2>
+        <h3>description: {{ description }}</h3>
+        <h4>price: {{ price }}</h4>
+        <button @click="addItemToCart(productName)"></button>
+    </div>
+</template>
