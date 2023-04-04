309e24bfa6248cfa9405e4649754fe105a272c10
Sarene Syed <sarene@Sarenes-MacBook-Air.local>
2023-04-04 09:40:53 -0400

    next

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -4,16 +4,16 @@ import CartItem from '.CartItem.vue'
 
 
 export default {
-    components: {
-        NavBar,
-        CartItem,
-    },
     data() {
         return {
             hotdog: "default",
             itemCount: 0,
         }
-    }
+    },
+    components: {
+        NavBar,
+        CartItem,
+    },
 }
 
 
@@ -21,43 +21,38 @@ export default {
 </script>
 <template>
     <body>
-        <NavBar></NavBar>
-        <img :src="src" alt="hotdog">
-        <div class="links">
-        </div>
-
-        <CartItem>
+        <CartItem
                 image = '../assets/vintage.png' 
                 productName = "Vintage Hot Dog"
                 description = "A hotdog dressed up in vintage clothing"
                 price = "2,000,000"
 
                 @add-to-cart="$event => itemCount++"
-        </CartItem>
-        <CartItem>
+        />
+        <CartItem
                 image = '../assets/patriotic.png' 
                 productName = "Patriotic Hot Dog"
                 description = "A hotdog patriotic enough for the fourth of july"
                 price = "10,000,000"
 
                 @add-to-cart="$event => itemCount++"
-        </CartItem>
-        <CartItem>
+        />
+        <CartItem
                 image = '../assets/beautiful.png' 
                 productName = "Beautiful Hot Dog"
                 description = "A hotdog all glammed up"
                 price = "1,000,000"
 
                 @add-to-cart="$event => itemCount++"
-        </CartItem>
-        <CartItem>
+        />
+        <CartItem
                 image = '../assets/winner.png' 
                 productName = "Winner Hot Dog"
                 description = "A hotdog who has beaten Michael Phelps and is proud of it"
                 price = "700,000,000"
 
                 @add-to-cart="$event => itemCount++"
-        </CartItem>
+        />
 
     </body>
     The amount of hotdogs in your cart totals to {{ itemCount }}
